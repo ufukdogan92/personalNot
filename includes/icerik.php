2c953b5f7580921a9b202f9e254ae12ee7780026
Sinan Ülker <unicod3@hotmail.com>
2014-12-20 20:15:39 +0200

    Update icerik.php
    short tag kullanımı kapalı ise sorun yaratabileceğinden kaldırıldı.

--- includes/icerik.php
+++ includes/icerik.php
@@ -1,35 +1,20 @@
-<div class="container theme-showcase upper" role="main">
-
-
+<div class="container theme-showcase upper" role="main"> 
     <div class="row">
-        <div class="col-md-9">
-
-            <?php
-
+        <div class="col-md-9"> 
+            <?php 
                 $sql = mysql_query("select * from notlar order by id DESC  limit 0,2");
                 while($sonuc = mysql_fetch_array($sql)){
                     $id = $sonuc["dersID"];
                     $sql2 = mysql_query("select * from dersler where id = $id");
                     $sonuc2 = mysql_fetch_array($sql2);
-            ?>
-
-
-
+            ?> 
             <div class="col-md-11 notlarIndex">
-                <h2><?=$sonuc["baslik"]?>  <span class="aTitle"><a href="#"><?=$sonuc2["dersAdi"]?></a></span> </h2>
-                <p> <?=$sonuc["kisaBaslik"]?></p>
+                <h2><?php echo $sonuc["baslik"]; ?>  <span class="aTitle"><a href="#"><?php echo $sonuc2["dersAdi"]; ?></a></span> </h2>
+                <p> <?php echo $sonuc["kisaBaslik"]; ?></p>
                 <p><a class="btn btn-default" href="#" role="button">Sayfaya Git &raquo;</a></p>
-            </div>
-
-
-            <?php } ?>
-
-
-        </div>
-
-    <?php include "includes/sag.php" ?>
-
-
-    </div>
-
+            </div> 
+            <?php } ?> 
+        </div> 
+    <?php include "includes/sag.php" ?>  
+    </div> 
 </div>
